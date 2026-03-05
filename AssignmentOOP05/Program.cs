6950905dd3d672e73657a47f5181002a3e58c4f6
Mira Elbasha <Mira2malak@gmail.com>
2026-03-05 14:57:41 +0200

    Part1:Q1

--- AssignmentOOP05/Program.cs
+++ AssignmentOOP05/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part1
+            #region Q1
+            //An interface in C# is a contract that defines a set of methods, properties..etc without implementing them. It specifies what a class must do
+            //we use it to reduce dependency and to achieve abstraction and polymorphism in our code also Instead of depending on a specific class, we depend on a contract (interface).
+
+            /*
+              1- loose coupling : which makes the system easier to modify becuase classes depends on interfaces not on specific implementations
+              2- Multiple Implementation : Different classes can implement the same interface in different ways.
+              3- Better Maintainability : Changes to the implementation of an interface do not affect the classes that use it
+            */
+            #endregion
+            #endregion
+
         }
     }
 }
